ef14e42fd516129e44ecb0a04ca9e8ad38179612
Esteban Torres <me@estebantorr.es>
2020-09-08 21:28:28 +0200

    Simplifies adr script

--- scripts/adr.sh
+++ scripts/adr.sh
@@ -1,49 +1,6 @@
-# #!/bin/bash
+#!/bin/bash
 
-docker=""
-old_adr=""
-title=""
-
-# Read arguments
-while getopts ":d:t:c" opt; do
-  case ${opt} in
-    t )
-      title=$OPTARG
-      ;;
-    d )
-      docker=$OPTARG
-      ;;
-    c )
-      old_adr=$OPTARG
-      ;;
-    \? )
-      echo "Invalid option: $OPTARG" 1>&2
-      ;;
-    : )
-      echo "Invalid option: $OPTARG requires an argument" 1>&2
-      ;;
-  esac
-done
-shift $((OPTIND -1))
-
-
-# Validations
-if [ -z "$docker" ]
-then
-      echo "\$docker is empty" || exit 1
-fi
-
-if [ -z "$title" ]
-then
-      echo "\$title is empty" || exit 1
-fi
-
-if [ -z "$" ]
-then
-      echo "\$old_adr is empty" || exit 1
-fi
-
-cmd=$(${docker} adr new -s ${old_adr} ${title})
-echo $cmd
-eval $cmd
-
+docker="docker run -it -v `PWD`:/plonk -w /plonk brianskarda/adr-tools-docker:latest"
+cmd="$docker adr $@"
+echo "$cmd"
+eval "$cmd" || exit 0
