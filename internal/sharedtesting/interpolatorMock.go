f14257382d1f776ff0952cdb3d35db923a80acc6
Esteban Torres <me@estebantorr.es>
2020-09-16 09:43:16 +0200

    Fixes interpolator mock

--- internal/sharedtesting/interpolatorMock.go
+++ internal/sharedtesting/interpolatorMock.go
@@ -8,7 +8,13 @@ type InterpolatorMock struct {
 }
 
 // SubstituteValues mocks SubstituteValues
-func (r *InterpolatorMock) SubstituteValues(source map[string]string, target string) (string, error) {
-	args := r.Called(source, target)
+func (i *InterpolatorMock) SubstituteValues(source map[string]string, target string) (string, error) {
+	args := i.Called(source, target)
 	return args.String(0), args.Error(1)
+}
+
+// SubstituteValuesInMap …
+func (i *InterpolatorMock) SubstituteValuesInMap(source map[string]string, target map[string]string) (map[string]string, error) {
+	args := i.Called(source, target)
+	return args.Get(0).(map[string]string), args.Error(1)
 }
