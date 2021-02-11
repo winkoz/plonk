0b36cf024bf3f8606f54d7fc223b63260a3ee122
Esteban Torres <me@estebantorr.es>
2021-02-11 23:24:43 +0100

    Adds ability to load dashboards from files Using the config map we can now specify dashboards in the template and they will be loaded into Grafana upon deploy.

---
+++ internal/io/yamlReader_test.go
@@ -0,0 +1,68 @@
+package io
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_yamlReader_Parse(t *testing.T) {
+	service := NewService()
+	output := []interface{}{}
+	type fields struct {
+		service Service
+	}
+	type args struct {
+		data   []byte
+		output interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name: "parses array of maps",
+			fields: fields{
+				service: service,
+			},
+			args: args{
+				data:   []byte("[{key1: test, key2: test, key3: test}, {key1: test, key2: test, key3: test}, {key1: test, key2: test, key3: test}]"),
+				output: output,
+			},
+			want: &[]map[string]string{
+				{
+					"key1": "test",
+					"key2": "test",
+					"key3": "test",
+				},
+				{
+					"key1": "test",
+					"key2": "test",
+					"key3": "test",
+				},
+				{
+					"key1": "test",
+					"key2": "test",
+					"key3": "test",
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			yr := yamlReader{
+				service: tt.fields.service,
+			}
+			err := yr.Parse(tt.args.data, tt.args.output)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("yamlReader.Parse() error = %v, wantErr %v", err, tt.wantErr)
+			} else {
+				assert.Equal(t, tt.want, tt.args.output)
+			}
+		})
+	}
+}
