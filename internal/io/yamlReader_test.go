6f97ad4e6851370d6b25a08bb40486130bd8a1b4
Esteban Torres <me@estebantorr.es>
2021-02-14 17:01:58 +0100

    Forces type on tests

--- internal/io/yamlReader_test.go
+++ internal/io/yamlReader_test.go
@@ -8,19 +8,19 @@ import (
 
 func Test_yamlReader_Parse(t *testing.T) {
 	service := NewService()
-	output := []interface{}{}
+	output := []map[string]string{}
 	type fields struct {
 		service Service
 	}
 	type args struct {
 		data   []byte
-		output interface{}
+		output []map[string]string
 	}
 	tests := []struct {
 		name    string
 		fields  fields
 		args    args
-		want    interface{}
+		want    []map[string]string
 		wantErr bool
 	}{
 		{
@@ -32,7 +32,7 @@ func Test_yamlReader_Parse(t *testing.T) {
 				data:   []byte("[{key1: test, key2: test, key3: test}, {key1: test, key2: test, key3: test}, {key1: test, key2: test, key3: test}]"),
 				output: output,
 			},
-			want: &[]map[string]string{
+			want: []map[string]string{
 				{
 					"key1": "test",
 					"key2": "test",
@@ -57,7 +57,7 @@ func Test_yamlReader_Parse(t *testing.T) {
 			yr := yamlReader{
 				service: tt.fields.service,
 			}
-			err := yr.Parse(tt.args.data, tt.args.output)
+			err := yr.Parse(tt.args.data, &tt.args.output)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("yamlReader.Parse() error = %v, wantErr %v", err, tt.wantErr)
 			} else {
