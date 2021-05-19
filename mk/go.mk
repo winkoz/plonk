82c2daa177c1a775c70819454f23d2df5c75c12f
Esteban Torres <me+github@estebantorr.es>
2021-05-19 21:29:36 +0200

    Consolidates pipelines

--- mk/go.mk
+++ mk/go.mk
@@ -6,7 +6,7 @@ BINARY_NAME?=plonk
 go-build-assets:
 	GO111MODULE=on $(GO-BIN-FOLDER)go-bindata -prefix "data/" -pkg data -o data/data.go data/...
 
-go-build: clean go-build-assets
+go-build: go-build-assets
 	GO111MODULE=on GOOS=$(GOOS) GOARCH=$(GOARCH) go build -mod=mod -ldflags="-s -w" -o bin/$(BINARY_NAME) main.go
 	@echo "Plonk built successfully!"
 
