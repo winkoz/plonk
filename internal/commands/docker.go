c3702dd3d99bd79300d7467243ec8679d16570e1
Esteban Torres <me+github@estebantorr.es>
2022-11-17 08:15:15 +0100

    Removes unnecessary platforms

--- internal/commands/docker.go
+++ internal/commands/docker.go
@@ -29,7 +29,7 @@ func (d dockerCommand) Build(tagName string, skipCache bool) error {
 	}
 
 	args = append(args, tags...)
-	args = append(args, "--platform", "linux/arm64,linux/amd64,linux/amd64/v2,linux/ppc64le,linux/s390x")
+	args = append(args, "--platform", "linux/arm64,linux/amd64,linux/amd64/v2")
 	args = append(args, ".")
 
 	_, err := d.executeCommand("Build", args...)
